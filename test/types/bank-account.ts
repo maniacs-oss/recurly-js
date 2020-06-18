7d532d7d3e082dd54698a37579ae13f87647ab4d
Caleb Barde <cbarde@recurly.com>
2020-06-18 16:42:51 -0600

    typescript support

--- test/types/bank-account.ts
+++ test/types/bank-account.ts
@@ -44,4 +44,46 @@ export default function bankAccount() {
       token.type;
     }
   });
+
+  const minimalBecsBillingInfo = {
+    type: "becs",
+    account_number: "1234",
+    account_number_confirmation: "1234",
+    branch_code: "1234",
+    nameOnAccount: "1234",
+  };
+
+  window.recurly.bankAccount.token(minimalBecsBillingInfo, (err, token) => {
+    if (err) {
+      err.message;
+      err.code;
+    } else {
+      token.id;
+      token.type;
+    }
+  });
+
+  const addressBecsBillingInfo = {
+    type: "becs",
+    account_number: "1234",
+    account_number_confirmation: "1234",
+    branch_code: "1234",
+    address1: "asdf",
+    address2: "asdf",
+    city: "asdf",
+    state: "asdf",
+    postal_code: "asdf",
+    country: "asdf",
+    vat_number: "asdf"
+  };
+
+  window.recurly.bankAccount.token(addressBecsBillingInfo, (err, token) => {
+    if (err) {
+      err.message;
+      err.code;
+    } else {
+      token.id;
+      token.type;
+    }
+  });
 }
--- types/lib/bank-account.d.ts
+++ types/lib/bank-account.d.ts
@@ -2,12 +2,13 @@ import { RecurlyError } from './error';
 import { TokenHandler } from './token';
 
 export type BillingInfo = {
-  routing_number: string;
-  account_number: string;
-  account_number_confirmation: string;
-  account_type: string;
+  routing_number?: string;
+  account_number?: string;
+  account_number_confirmation?: string;
+  account_type?: string;
+  branch_code?: string;
   iban?: string;
-  name_on_account: string;
+  name_on_account?: string;
   address1?: string;
   address2?: string;
   city?: string;
